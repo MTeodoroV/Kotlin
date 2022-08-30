9fbebc087165ad7247feb8df067cb2ee2c8fef1c
Mateus <mateusteodorovieirabaptista@gmail.com>
2022-08-30 15:29:34 -0300

    Criando a pagina de formulario.

--- OrganizerList/app/src/main/java/br/com/organizerlist/model/Product.kt
+++ OrganizerList/app/src/main/java/br/com/organizerlist/model/Product.kt
@@ -3,9 +3,9 @@ package br.com.organizerlist.model
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import br.com.organizerlist.R
-import java.math.BigDecimal
+import java.math.BigInteger
 
 data class Product(
     val name: String,
-    val qty: Int
+    val qty: BigInteger
 )
---
+++ OrganizerList/app/src/main/java/br/com/organizerlist/ui/activity/ProductForm.kt
@@ -0,0 +1,38 @@
+package br.com.organizerlist.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import br.com.organizerlist.R
+import br.com.organizerlist.model.Product
+import java.math.BigInteger
+
+class ProductForm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_form)
+
+        val ButtonAdd = findViewById<Button>(R.id.addProduct)
+        ButtonAdd.setOnClickListener{
+            val fieldName = findViewById<EditText>(R.id.inputProductName)
+            val name = fieldName.text.toString()
+            val getFieldQty = findViewById<EditText>(R.id.inputProductQty)
+            val fieldQty = getFieldQty.text.toString()
+            val qty = if(fieldQty.isBlank()) {
+                BigInteger.ZERO
+            } else {
+                BigInteger(fieldQty)
+            }
+
+
+            val productNew = Product(
+                name = name,
+                qty = qty
+            )
+
+            Log.i("FormularioProduto", "onCreate: $productNew")
+        }
+    }
+}
